bc36e7b3637d2c45ca63a8b48abcc9b29c656fa1
dianamgrg <dianamgrg@users.noreply.github.com>
2017-01-06 16:24:05 +0200

    Add files via upload
    +Optiuni

--- Source1.cpp
+++ Source1.cpp
@@ -16,19 +16,22 @@ void CreateTextTextures();
 void Render();
 void Menu();
 
-TTF_Font* font;
+TTF_Font* font_Title;
+TTF_Font* font_Options;
 SDL_Color textColor = { 255, 255, 255, 255 };
 SDL_Color backgroundColor = { 0, 0, 0, 255 };
 
-SDL_Texture* solidTexture;
-SDL_Texture* blendedTexture;
-SDL_Texture* shadedTexture;
+SDL_Texture* blendedTexture_Title;
+SDL_Texture* blendedTexture_Play;
+SDL_Texture* blendedTexture_HiSc;
+SDL_Texture* blendedTexture_Exit;
 
-SDL_Rect solidRect;
-SDL_Rect blendedRect;
-SDL_Rect shadedRect;
+SDL_Rect blendedRect_Title;
+SDL_Rect blendedRect_Play;
+SDL_Rect blendedRect_HiSc;
+SDL_Rect blendedRect_Exit;
 
-SDL_Rect windowRect = { SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720 };
+SDL_Rect windowRect = { SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 500 };
 SDL_Window* window;
 SDL_Renderer* renderer;
 
@@ -68,7 +71,10 @@ void Render()
 {
 	SDL_RenderClear(renderer);
 
-	SDL_RenderCopy(renderer, blendedTexture, nullptr, &blendedRect);
+	SDL_RenderCopy(renderer, blendedTexture_Title, nullptr, &blendedRect_Title);
+	SDL_RenderCopy(renderer, blendedTexture_Play, nullptr, &blendedRect_Play);
+	SDL_RenderCopy(renderer, blendedTexture_HiSc, nullptr, &blendedRect_HiSc);
+	SDL_RenderCopy(renderer, blendedTexture_Exit, nullptr, &blendedRect_Exit);
 
 	SDL_RenderPresent(renderer);
 }
@@ -80,8 +86,14 @@ bool SetupTTF(const std::string &fontName)
 		cout << " Failed to initialize TTF : " << SDL_GetError() << std::endl;
 		return false;
 	}
-	font = TTF_OpenFont(fontName.c_str(), 90);
-	if (font == nullptr)
+	font_Title = TTF_OpenFont(fontName.c_str(), 90);
+	if (font_Title == nullptr)
+	{
+		cout << " Failed to load font : " << SDL_GetError() << std::endl;
+		return false;
+	}
+	font_Options = TTF_OpenFont(fontName.c_str(), 50);
+	if (font_Options == nullptr)
 	{
 		cout << " Failed to load font : " << SDL_GetError() << std::endl;
 		return false;
@@ -92,12 +104,37 @@ bool SetupTTF(const std::string &fontName)
 
 void CreateTextTextures()
 {
-	SDL_Surface* blended = TTF_RenderText_Blended(font, "*FLAPPY BIRD*", textColor);
-	blendedTexture = SurfaceToTexture(blended);
+	//Title
+	SDL_Surface* blended_Title = TTF_RenderText_Blended(font_Title, "*FLAPPY BIRD*", textColor);
+	blendedTexture_Title = SurfaceToTexture(blended_Title);
 
-	SDL_QueryTexture(blendedTexture, NULL, NULL, &blendedRect.w, &blendedRect.h);
-	blendedRect.x = (windowRect.w - blendedRect.w)/2;
-	blendedRect.y = solidRect.y + solidRect.h + 20;
+	SDL_QueryTexture(blendedTexture_Title, NULL, NULL, &blendedRect_Title.w, &blendedRect_Title.h);
+	blendedRect_Title.x = (windowRect.w - blendedRect_Title.w)/2;
+	blendedRect_Title.y = 75;
+
+	//Play
+	SDL_Surface* blended_Play = TTF_RenderText_Blended(font_Options, "Play", textColor);
+	blendedTexture_Play = SurfaceToTexture(blended_Play);
+
+	SDL_QueryTexture(blendedTexture_Play, NULL, NULL, &blendedRect_Play.w, &blendedRect_Play.h);
+	blendedRect_Play.x = (windowRect.w - blendedRect_Play.w) / 2;
+	blendedRect_Play.y = blendedRect_Title.y + blendedRect_Title.h + 10;
+
+	//Highscores
+	SDL_Surface* blended_HiSc = TTF_RenderText_Blended(font_Options, "Highscores", textColor);
+	blendedTexture_HiSc = SurfaceToTexture(blended_HiSc);
+
+	SDL_QueryTexture(blendedTexture_HiSc, NULL, NULL, &blendedRect_HiSc.w, &blendedRect_HiSc.h);
+	blendedRect_HiSc.x = (windowRect.w - blendedRect_HiSc.w) / 2;
+	blendedRect_HiSc.y = blendedRect_Play.y + blendedRect_Play.h + 10;
+
+	//Exit
+	SDL_Surface* blended_Exit = TTF_RenderText_Blended(font_Options, "Exit", textColor);
+	blendedTexture_Exit = SurfaceToTexture(blended_Exit);
+
+	SDL_QueryTexture(blendedTexture_Exit, NULL, NULL, &blendedRect_Exit.w, &blendedRect_Exit.h);
+	blendedRect_Exit.x = (windowRect.w - blendedRect_Exit.w) / 2;
+	blendedRect_Exit.y = blendedRect_HiSc.y + blendedRect_HiSc.h + 10;
 }
 
 SDL_Texture* SurfaceToTexture(SDL_Surface* surf)
@@ -178,6 +215,7 @@ void CloseEverything()
 {
 	SDL_DestroyWindow(window);
 	window = NULL;
-	TTF_CloseFont(font);
+	TTF_CloseFont(font_Title);
+	TTF_CloseFont(font_Options);
 	SDL_Quit();
 }
